b8b36e5653b146fce46acde4017794414bf85f94
victor moreira <victorfranklyn@poli.ufrj.br>
2018-11-20 17:38:56 -0200

    Definicao dos pinos

--- multiplicador/multiplicador.ino
+++ multiplicador/multiplicador.ino
@@ -1,6 +1,52 @@
+#define PIN_BOTAO_1 1
+#define PIN_BOTAO_2 2
+
+//Pinos do display esquerdo
+//Mais significativo
+#define PIN_LED_1A 3
+#define PIN_LED_1B ( PIN_LED_1A + 1 )
+#define PIN_LED_1C ( PIN_LED_1B + 1 )
+#define PIN_LED_1D ( PIN_LED_1C + 1 )
+#define PIN_LED_1E ( PIN_LED_1D + 1 )
+#define PIN_LED_1F ( PIN_LED_1E + 1 )
+#define PIN_LED_1G ( PIN_LED_1F + 1 )
+
+//Pinos do display direito
+//Menos significativo
+#define PIN_LED_2A 3
+#define PIN_LED_2B ( PIN_LED_2A + 1 )
+#define PIN_LED_2C ( PIN_LED_2B + 1 )
+#define PIN_LED_2D ( PIN_LED_2C + 1 )
+#define PIN_LED_2E ( PIN_LED_2D + 1 )
+#define PIN_LED_2F ( PIN_LED_2E + 1 )
+#define PIN_LED_2G ( PIN_LED_2F + 1 )
+
 void setup()
 {
+  // Botoes
+  pinMode( PIN_BOTAO_1, INPUT);
+  pinMode( PIN_BOTAO_2, INPUT);
   
+  // Display esquerdo 
+  //( mais significativo )
+  pinMode(PIN_LED_1A, OUTPUT);
+  pinMode(PIN_LED_1B, OUTPUT);
+  pinMode(PIN_LED_1C, OUTPUT);
+  pinMode(PIN_LED_1D, OUTPUT);
+  pinMode(PIN_LED_1E, OUTPUT);
+  pinMode(PIN_LED_1F, OUTPUT);
+  pinMode(PIN_LED_1G, OUTPUT);
+
+  // Display direiro
+  // Menos Significativo
+
+  pinMode(PIN_LED_2A, OUTPUT);
+  pinMode(PIN_LED_2B, OUTPUT);
+  pinMode(PIN_LED_2C, OUTPUT);
+  pinMode(PIN_LED_2D, OUTPUT);
+  pinMode(PIN_LED_2E, OUTPUT);
+  pinMode(PIN_LED_2F, OUTPUT);
+  pinMode(PIN_LED_2G, OUTPUT);
 }
 
 void loop()
